5df89a62a8174319dd8cc7b4c9f52cb1a0c2ebc4
Akeem Mohammed <60115485+ak55m@users.noreply.github.com>
2026-04-13 20:21:42 -0500

    Unify flight crash menu joystick controls

--- Assets/Scripts/DeathUIManager.cs
+++ Assets/Scripts/DeathUIManager.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -24,8 +22,18 @@ public class DeathUIManager : MonoBehaviour
     public CollisionDeathHandler deathHandler;
     private CanvasGroup deathPanelGroup;
     private bool isRestarting = false;
-    private bool js0WasPressed = false;
     private FlyController fly;
+
+    public float stickThreshold = 0.5f;
+    public float navigationCooldown = 0.3f;
+
+    private int _selectedIndex = 0;
+    private const int OPTION_COUNT = 2;
+    private bool _stickNeutral = true;
+    private float _cooldownTimer = 0f;
+
+    private static readonly Color SELECTED_COLOR = new Color(1f, 0.85f, 0f);
+    private static readonly Color UNSELECTED_COLOR = Color.white;
 
     void Start()
     {
@@ -45,35 +53,33 @@ public class DeathUIManager : MonoBehaviour
             if (btnText) btnText.text = restartLabel;
             restartButton.onClick.AddListener(OnRestartClicked);
         }
+        else
+        {
+            Debug.LogError("restartButton is NULL");
+        }
 
         if (backButton)
         {
             backButton.onClick.AddListener(OnBackClicked);
             var backBtnText = backButton.GetComponentInChildren<TextMeshProUGUI>();
             if (backBtnText) backBtnText.text = "Back to Museum";
-            Debug.Log("Back button listener added");
-        }else{
+        }
+        else
+        {
             Debug.LogWarning("backButton is NULL");
         }
-
-        if (restartButton)
-        {
-            restartButton.onClick.AddListener(OnRestartClicked);
-            Debug.Log("Button listener added");
-        }else{
-            Debug.LogError("restartButton is NULL");
-        }
-
-
     }
 
     void Update()
     {
-        if (deathPanelGroup.alpha <= 0f) {
-		return;
-	}
+        if (deathPanelGroup.alpha <= 0f)
+        {
+            return;
+        }
 
-        HandleGazeAndController();
+        _cooldownTimer -= Time.unscaledDeltaTime;
+        HandleNavigation();
+        HandleConfirm();
     }
 
     public void OnBackClicked()
@@ -106,51 +112,85 @@ public class DeathUIManager : MonoBehaviour
         deathPanelGroup.blocksRaycasts = true;
 
         UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
+
+        _selectedIndex = 0;
+        _stickNeutral = true;
+        _cooldownTimer = 0f;
+        UpdateHighlight();
     }
 
     public void HideDeathScreen()
     {
+        _stickNeutral = true;
+        _cooldownTimer = 0f;
         deathPanelGroup.alpha = 0f;
         deathPanelGroup.interactable = false;
         deathPanelGroup.blocksRaycasts = false;
     }
 
-    void HandleGazeAndController()
+    private void HandleNavigation()
     {
-        PointerEventData pointerData = new PointerEventData(EventSystem.current)
+        float axis = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(axis) < stickThreshold)
         {
-            position = new Vector2(Screen.width / 2f, Screen.height / 2f)
-        };
+            _stickNeutral = true;
+            return;
+        }
 
-        List<RaycastResult> results = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(pointerData, results);
-
-        GameObject gazedButton = null;
-
-        foreach (var r in results)
+        if (!_stickNeutral || _cooldownTimer > 0f)
         {
-            Button btn = r.gameObject.GetComponent<Button>();
-            if (btn != null)
-            {
-                gazedButton = r.gameObject;
+            return;
+        }
+
+        if (axis > stickThreshold)
+        {
+            _selectedIndex = (_selectedIndex - 1 + OPTION_COUNT) % OPTION_COUNT;
+        }
+        else
+        {
+            _selectedIndex = (_selectedIndex + 1) % OPTION_COUNT;
+        }
+
+        _stickNeutral = false;
+        _cooldownTimer = navigationCooldown;
+        UpdateHighlight();
+    }
+
+    private void HandleConfirm()
+    {
+        if (!Input.GetKeyDown(KeyCode.JoystickButton2))
+        {
+            return;
+        }
+
+        switch (_selectedIndex)
+        {
+            case 0:
+                OnRestartClicked();
                 break;
-            }
+            case 1:
+                OnBackClicked();
+                break;
         }
+    }
 
-        if (gazedButton != null)
+    private void UpdateHighlight()
+    {
+        SetButtonColor(restartButton, _selectedIndex == 0 ? SELECTED_COLOR : UNSELECTED_COLOR);
+        SetButtonColor(backButton, _selectedIndex == 1 ? SELECTED_COLOR : UNSELECTED_COLOR);
+    }
+
+    private void SetButtonColor(Button btn, Color color)
+    {
+        if (btn == null)
         {
-            Debug.Log("Looking at:" + gazedButton.name);
-
-            if (Input.GetButtonDown("js0"))
-            {
-                Debug.Log("js0 pressed on:" + gazedButton.name);
-
-                Button btn = gazedButton.GetComponent<Button>();
-                if (btn != null){
-                    btn.onClick.Invoke();
-		}
-            }
+            return;
         }
+
+        var c = btn.colors;
+        c.normalColor = color;
+        btn.colors = c;
     }
 
     void LockToCamera()
